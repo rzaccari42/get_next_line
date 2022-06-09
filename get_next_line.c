e5c3a3d6719ea33b2181ceb0d0ff786ec706a047
Raphael Zaccaria <raphzer@macbook-pro-de-raphael.home>
2022-06-09 23:44:07 +0200

    gnl ok

---
+++ get_next_line.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 17:27:55 by rzaccari          #+#    #+#             */
+/*   Updated: 2022/06/09 23:19:47 by rzaccari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/get_next_line.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd)
+{
+	static char	*string = NULL;
+	char		buffer[BUFFER_SIZE + 1];
+	int			read_bytes;
+
+	if (fd < 0 || fd > 4095 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (string && has_line(string) == 1)
+		return (get_line(&string));
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	if (read_bytes == -1)
+	{
+		free(string);
+		return (NULL);
+	}
+	buffer[read_bytes] = '\0';
+	while (read_bytes > 0)
+	{
+		string = ft_strjoin(string, buffer);
+		if (has_line(string) == 1)
+			break;
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		buffer[read_bytes] = '\0';
+	}
+	return(get_line(&string));
+}
+
+char	*get_line(char **str)
+{
+	char	*line;
+	char	*temp;
+	int		i;
+
+	i = 0;
+	if (str == NULL || *str == NULL)
+		return (NULL);
+	while ((*str)[i] != '\0' && (*str)[i] != '\n')
+		i++;
+	line = ft_strndup(*str, 0, i);
+	if (has_line(*str) == 0)
+		temp = NULL;
+	else
+		temp = ft_strndup(*str, i + 1, ft_strlen(*str));
+	free(*str);
+	*str = temp;
+	return (line);
+}
+
+int	has_line(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 18:59:16 by rzaccari          #+#    #+#             */
+/*   Updated: 2022/02/07 20:38:34 by rzaccari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*get_line(char **str);
+int		has_line(char *str);
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_strndup(char *str, int start, int end);
+
+#endif
